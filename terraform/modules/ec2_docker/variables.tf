864eb2702168eb6d504b5495009b2692d4a11b5a
Danilo Sibov <danilo.sibov@gmail.com>
2026-09-21 13:52:00 -0300

    Arrumar Docker

--- terraform/modules/ec2_docker/variables.tf
+++ terraform/modules/ec2_docker/variables.tf
@@ -1,9 +1,33 @@
-variable "project_name" { type = string }
-variable "environment" { type = string }
-variable "instance_type" { type = string }
-variable "vpc_id" { type = string, default = null }
-variable "subnet_id" { type = string, default = null }
+variable "project_name" {
+  type = string
+}
 
-variable "github_repo_url" { type = string }
-variable "env_file_content" { type = string }
-variable "google_credentials_content" { type = string }
+variable "environment" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "vpc_id" {
+  type    = string
+  default = null
+}
+
+variable "subnet_id" {
+  type    = string
+  default = null
+}
+
+variable "github_repo_url" {
+  type = string
+}
+
+variable "env_file_content" {
+  type = string
+}
+
+variable "google_credentials_content" {
+  type = string
+}
